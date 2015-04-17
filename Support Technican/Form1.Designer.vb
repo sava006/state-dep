632615047ec811d64615f5abe406ac4e537c7250
sava0066 <harrysavagespeaks@gmail.com>
2015-04-17 18:16:30 -0500

    this a commit

---
+++ Support Technican/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTechName = New System.Windows.Forms.TextBox()
+        Me.txtTechEmail = New System.Windows.Forms.TextBox()
+        Me.grdTechnicians = New System.Windows.Forms.DataGridView()
+        Me.btnAddTech = New System.Windows.Forms.Button()
+        Me.btnEditTech = New System.Windows.Forms.Button()
+        Me.btnEmailTech = New System.Windows.Forms.Button()
+        Me.cboTechRole = New System.Windows.Forms.ComboBox()
+        CType(Me.grdTechnicians, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "email"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Role"
+        '
+        'txtTechName
+        '
+        Me.txtTechName.Location = New System.Drawing.Point(59, 25)
+        Me.txtTechName.Name = "txtTechName"
+        Me.txtTechName.Size = New System.Drawing.Size(121, 20)
+        Me.txtTechName.TabIndex = 3
+        '
+        'txtTechEmail
+        '
+        Me.txtTechEmail.Location = New System.Drawing.Point(59, 61)
+        Me.txtTechEmail.Name = "txtTechEmail"
+        Me.txtTechEmail.Size = New System.Drawing.Size(121, 20)
+        Me.txtTechEmail.TabIndex = 4
+        '
+        'grdTechnicians
+        '
+        Me.grdTechnicians.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdTechnicians.Location = New System.Drawing.Point(197, 25)
+        Me.grdTechnicians.Name = "grdTechnicians"
+        Me.grdTechnicians.Size = New System.Drawing.Size(240, 150)
+        Me.grdTechnicians.TabIndex = 6
+        '
+        'btnAddTech
+        '
+        Me.btnAddTech.Location = New System.Drawing.Point(59, 148)
+        Me.btnAddTech.Name = "btnAddTech"
+        Me.btnAddTech.Size = New System.Drawing.Size(102, 23)
+        Me.btnAddTech.TabIndex = 7
+        Me.btnAddTech.Text = "Add Technician"
+        Me.btnAddTech.UseVisualStyleBackColor = True
+        '
+        'btnEditTech
+        '
+        Me.btnEditTech.Location = New System.Drawing.Point(59, 207)
+        Me.btnEditTech.Name = "btnEditTech"
+        Me.btnEditTech.Size = New System.Drawing.Size(102, 23)
+        Me.btnEditTech.TabIndex = 8
+        Me.btnEditTech.Text = "Edit Technician"
+        Me.btnEditTech.UseVisualStyleBackColor = True
+        '
+        'btnEmailTech
+        '
+        Me.btnEmailTech.Location = New System.Drawing.Point(197, 207)
+        Me.btnEmailTech.Name = "btnEmailTech"
+        Me.btnEmailTech.Size = New System.Drawing.Size(177, 23)
+        Me.btnEmailTech.TabIndex = 9
+        Me.btnEmailTech.Text = "email selected technicians"
+        Me.btnEmailTech.UseVisualStyleBackColor = True
+        '
+        'cboTechRole
+        '
+        Me.cboTechRole.FormattingEnabled = True
+        Me.cboTechRole.Location = New System.Drawing.Point(59, 90)
+        Me.cboTechRole.Name = "cboTechRole"
+        Me.cboTechRole.Size = New System.Drawing.Size(121, 21)
+        Me.cboTechRole.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 262)
+        Me.Controls.Add(Me.cboTechRole)
+        Me.Controls.Add(Me.btnEmailTech)
+        Me.Controls.Add(Me.btnEditTech)
+        Me.Controls.Add(Me.btnAddTech)
+        Me.Controls.Add(Me.grdTechnicians)
+        Me.Controls.Add(Me.txtTechEmail)
+        Me.Controls.Add(Me.txtTechName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.grdTechnicians, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtTechName As System.Windows.Forms.TextBox
+    Friend WithEvents txtTechEmail As System.Windows.Forms.TextBox
+    Friend WithEvents grdTechnicians As System.Windows.Forms.DataGridView
+    Friend WithEvents btnAddTech As System.Windows.Forms.Button
+    Friend WithEvents btnEditTech As System.Windows.Forms.Button
+    Friend WithEvents btnEmailTech As System.Windows.Forms.Button
+    Friend WithEvents cboTechRole As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Support Technican/Form1.vb
@@ -0,0 +1,122 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim objConnection As New SqlConnection("server=HARRY-HP\SQLEXPRESS;database=support; Trusted_Connection=Yes")
+    Dim objDataAdapter As New SqlDataAdapter
+    Dim objDataTable As New DataTable
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        setupRolesComboBox()
+        loadTechnicianDataGridView()
+    End Sub
+
+    Private Sub setupRolesComboBox()
+        Dim roles As String() = {"PC Support", "Mac Support", "Server Support", "Network support"}
+        cboTechRole.Items.AddRange(roles)
+        cboTechRole.DropDownStyle = ComboBoxStyle.DropDownList
+    End Sub
+
+    Private Sub loadTechnicianDataGridView()
+        'use should only  select whole rows; and one at a time
+        grdTechnicians.MultiSelect = False
+        grdTechnicians.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+
+        'In this app, user will not be able to edit the table directly, they will be adding techicians using the text box/combo boxes
+        grdTechnicians.EditMode = DataGridViewEditMode.EditProgrammatically
+        'So hid the letfmost edit column (the row headers)
+        grdTechnicians.RowHeadersVisible = False
+        'Size the columns to fit the data they contain
+        grdTechnicians.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+
+        'Get data from DB and add it to Data  GridView
+        objDataAdapter = New SqlDataAdapter("Select * from technicians", objConnection)
+        objDataAdapter.Fill(objDataTable)
+        objConnection.Close()
+        'Fi we want to modify and/or delecte data, it's useful to have commandbuilder
+        'to automatically generate insert, update or delete statements for our data
+        Dim SqlCommandBuilder As New SqlCommandBuilder(objDataAdapter)
+        'Tell the DataGridView to use this DataTAble as its DataSource
+        grdTechnicians.DataSource = objDataTable
+
+    End Sub
+
+    Private Sub btnAddTech_Click(sender As Object, e As EventArgs) Handles btnAddTech.Click
+
+        'Read in data from text boxes and combobox; simplle validation
+        'other checks - is the email address is in a valid format?
+        'You can probably think of other  things to check
+        If txtTechEmail Is String.Empty Or txtTechName Is String.Empty Or
+            cboTechRole.SelectedItem Is Nothing Then
+
+            MessageBox.Show("Check you've entered all the data needed")
+
+        Else
+            'If the data is ok, add to and DataGridView(via DataTable)
+            '  adn to database (via DataTable and DataAdapter).
+
+            Dim newTechRow As DataRow = objDataTable.NewRow()
+            'Add the data from the textboxes, combobox. Use the column names
+            newTechRow("name") = txtTechName.Text
+            newTechRow("email") = txtTechEmail.Text
+            newTechRow("role") = cboTechRole.SelectedItem
+            'add to the datatable; this also updates the dataGrid
+            objDataTable.Rows.Add(newTechRow)
+            'tell the dataAdapter that the datatabble has changed and send the changes to the database 
+            objDataAdapter.Update(objDataTable)
+        End If
+    End Sub
+
+    Private Sub grdTechnicians_SelectionChanged(sender As Object, e As EventArgs) Handles grdTechnicians.SelectionChanged
+
+        'populates the textBoxes and Combobox with info from the selected row
+        'SelectedRows returns an array of rows. Since the DataGridView is set up
+        'to only allow one row to be selected, our selected row is element 0.
+
+        Dim row As DataGridViewRow = grdTechnicians.SelectedRows.Item(0)
+        'From the row, get each cell, get the formatted value, turn it to a string
+        'and use it to set the text or selected itme of the component
+        txtTechName.Text = row.Cells.Item(1).FormattedValue.ToString
+        txtTechEmail.Text = row.Cells.Item(2).FormattedValue.ToString
+        cboTechRole.SelectedItem = row.Cells.Item(3).FormattedValue.ToString
+    End Sub
+
+
+    Private Sub btnEditTech_Click(sender As Object, e As EventArgs) Handles btnEditTech.Click
+        Dim row As DataGridViewRow = grdTechnicians.SelectedRows.Item(0)
+        Dim techID As String = row.Cells.Item(0).FormattedValue.ToString
+
+        'Read in the values from the textboxes and combox. **this needs validation
+        Dim newName As String = txtTechName.Text
+        Dim newemail As String = txtTechEmail.Text
+        Dim newRole As String = cboTechRole.SelectedItem.ToString
+
+        Dim editTechrow As DataRow = objDataTable.Select("id = " & techID)(0)
+
+        'Use column values to update this row with the new data. IT doesn't if it's different or not.
+        'As this updates the DataTable, the DataGridView will see the changes and update too
+        editTechrow("name") = newName
+        editTechrow("email") = newemail
+        editTechrow("role") = newRole
+
+        'Request the dataAdapter updates the database
+        objDataAdapter.Update(objDataTable)
+
+    End Sub
+
+    Private Sub btnEmailTech_Click(sender As Object, e As EventArgs) Handles btnEmailTech.Click
+
+        'Get the selected row
+        Dim row As DataGridViewRow = grdTechnicians.SelectedRows.Item(0)
+        'from the row, get the second cell- the one with the email in
+        Dim techEmail As String = row.Cells.Item(2).FormattedValue.ToString
+        Dim subject As String = "Hello from the Technician Management Application!"
+        Try
+            Process.Start(String.Format("mailto:{0}?subject={1}", techEmail, subject))
+
+        Catch ex As Exception
+            MessageBox.Show("No default email client (e.g. Outlook) is configured")
+
+        End Try
+    End Sub
+End Class
